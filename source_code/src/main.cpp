411a5b77d7a1c018ee3fe24bfdcbdbafd5a87e7d
Matheus Cardoso Medeiros <matheuscardosomedeiros2@gmail.com>
2026-08-10 23:21:03 -0300

    Added function to read sensor

--- source_code/src/main.cpp
+++ source_code/src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 
+#define sensorPin 13
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(sensorPin, INPUT);
   int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (digitalRead(sensorPin)) {
+    Serial.println("Nível alto.");
+  }
 }
 
 // put function definitions here:
